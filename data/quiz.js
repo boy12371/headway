3746699657d13f8ca13f2514644e7a4effc13824
Simon Lang <simon.lang@cambia.org>
2018-05-02 13:35:42 +1000

    Sample data fixtures

---
+++ data/quiz.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        question: 'how old is the earth?',
+        answers: [
+            { text: '5 billion years', correct: true },
+            { text: 'really old', correct: true },
+            { text: '2000 years', correct: false },
+        ]
+
+    },
+    // ...
+]
