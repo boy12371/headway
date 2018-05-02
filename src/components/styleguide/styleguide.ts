14ac5b87b083090a45ccc1f5e2155a9e19897acd
Jason Corbett <jsncbt@gmail.com>
2018-05-02 14:11:53 +1000

    Remove dependencies

---
+++ src/components/styleguide/styleguide.ts
@@ -0,0 +1,13 @@
+import { Component, Vue } from 'vue-property-decorator'
+
+import './styleguide.scss'
+
+@Component({
+  template: require('./styleguide.html'),
+  components: {
+  }
+})
+
+export class StyleguideComponent extends Vue {
+
+}
