51e726fd8fe9f8e6ff31e883d5df627c5b54d459
Simon Lang <me@simonlang.org>
2018-05-04 13:24:40 +1000

    Student Interface example

--- server/models/Student.ts
+++ server/models/Student.ts
@@ -1,6 +1,15 @@
 import Sequelize from 'sequelize'
 import connection from '../connection'
 
+export interface IStudent {
+    id: Number
+    first_name: String
+    last_name: String
+    email: String
+    password: String
+    salt: String
+}
+
 const Student = connection.define('student', {
     first_name: Sequelize.STRING,
     last_name: Sequelize.STRING,
