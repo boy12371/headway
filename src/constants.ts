2b614a78cc5b0375cc4e788cc516740495a1374c
Simon Lang <simon.lang@cambia.org>
2018-05-11 08:01:57 +1000

    BASE_URL can be overwritten with localStorage

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,4 @@
-export const BASE_URL: string = window.location.origin || 'http://localhost:5000'
+// Overwrite in local dev with localStorage.setItem('BASE_URL', 'http://localhost:5000')
+export const BASE_URL: string = localStorage.getItem('BASE_URL')
+  || window.location.origin
+  || 'http://localhost:5000'
