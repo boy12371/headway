66d712f24bfde8ccefc28fdaca8e1e1a02718b98
Simon Lang <simon.lang@cambia.org>
2018-05-15 10:23:18 +1000

    Student routes mostly complete

--- server/routes/student.ts
+++ server/routes/student.ts
@@ -2,25 +2,51 @@ import app from '../app'
 
 import { checkStudentLogin, authStudent, checkStudentEnrolled } from '../authentication'
 
-import { Course, Student, Card } from '../models'
+import { Course, Student, Card, Unit, Business } from '../models'
 import { getStudentActivitiesByUnit, studentUnitProgress, incrementCompletedUnits } from '../actions'
 
-app.use('/student/*', checkStudentLogin)
+app.use('/student*', checkStudentLogin)
 
-app.get('/student/courses', (req, res) => {
-  Student.findById(req.user.student.id, { include: [Course] }).then(student => {
+app.get('/student', (req, res) => {
+  Student.scope('public').findById(req.user.student.id, {
+    include: [
+      Business
+    ]
+  }).then(student => res.send(student))
+})
+
+app.get('/student/course', (req, res) => {
+  Student.findById(req.user.student.id, {
+    include: [
+      { model: Course, include: [Unit] },
+    ]
+  }).then(student => {
     res.send(student.courses)
   })
 })
 
 app.get('/student/course/:courseId', checkStudentEnrolled, (req, res) => {
-  Course.findById(req.params.courseId).then(course => {
-    res.send(`Student ${req.user.student.displayName()} enrolled in course ${course.name}`)
+  Course.findById(req.params.courseId, {
+    include: [
+      { model: Unit, include: [Card] }
+    ]
+  }).then(course => {
+    res.send(course)
   })
 })
 
-app.post('/students/:cardId/submit', (req, res) => {
-  const { cardId } = res.body
+app.get('/student/activity', (req, res) => {
+  Student.findById(req.user.student.id, {
+    include: [
+      { model: Card },
+    ]
+  }).then(student => {
+    res.send(student.cardActivities)
+  })
+})
+
+app.post('/student/:cardId/submit', (req, res) => {
+  const { cardId } = res.body // params?
   Card.findById(cardId).then(card => {
     studentUnitProgress(card.unitId, cardId).then(progress => {
       if (progress.unitCompleted) {
