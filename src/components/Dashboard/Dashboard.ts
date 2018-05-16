cbf50eb31ca5bd942961fd1b03115ce03f3c2e43
Simon Lang <simon.lang@cambia.org>
2018-05-16 15:56:26 +1000

    Fix typo in course menu count

--- src/components/Dashboard/Dashboard.ts
+++ src/components/Dashboard/Dashboard.ts
@@ -60,7 +60,7 @@ export class Dashboard extends Vue {
         const data = {
           text: course.name,
           link: '/c/' + course.id,
-          totalUnits: course.unit ? course.units.length : 0,
+          totalUnits: course.units ? course.units.length : 0,
         }
         return data
       })
