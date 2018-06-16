458c611c682e9902d1e26bc3a89fd0d1cf2e50bc
Simon Lang <simon.lang@cambia.org>
2018-06-16 12:18:15 +1000

    Make db does not contain data by default
    To get sample data use:
    
    SAMPLE_DATA=1 make db

--- server/reset-database.ts
+++ server/reset-database.ts
@@ -1,6 +1,7 @@
 require('dotenv').config()
 import connection from './connection'
-import report from './report'
+import * as bcrypt from 'bcrypt'
+import { SALT_ROUNDS } from './constants'
 
 import { Business, Card, Course, Mentor, Student, Unit, Admin, BusinessStudent, BusinessCourse, CourseStudent, Activity } from './models'
 import { createAdmin, createStudent, createMentor } from './actions'
@@ -16,6 +17,20 @@ const done = () => {
 }
 
 const main = async () => {
+  if (!process.env.SAMPLE_DATA) {
+    // TODO: once admin registration exists, this can be removed
+    const salt = bcrypt.genSaltSync(SALT_ROUNDS)
+    const password = bcrypt.hashSync('password', salt)
+    Admin.create({
+      id: 1,
+      name: 'EZ Training',
+      email: 'root',
+      password,
+      salt,
+    }).then(done)
+    return
+  }
+  console.log('Loading sample data')
   await Promise.all([
     ...admins.map(admin => createAdmin(admin)),
     ...students.map(student => createStudent(student)),
@@ -30,7 +45,6 @@ const main = async () => {
     d.quiz = JSON.stringify(d.quiz)
     return Card.create(d)
   }))
-  // await Card.create({ name: 'Welcome', unitId: 1, evidence_task: 'Demonstrate a set of clean tools', quiz: JSON.stringify(quiz) })
   await createMentor({ first_name: 'Buddha', businessId: 1, email: 'buddha@gmail.com', password: 'password' })
   await createMentor({ first_name: 'Jesus', businessId: 1, email: 'jesus@hotmail.com', password: 'password' })
   await BusinessStudent.create({ businessId: 1, studentId: 1, })
@@ -43,7 +57,7 @@ const main = async () => {
   await CourseStudent.create({ courseId: 2, studentId: 1, assigned: Date.now() })
   await Activity.create({ studentId: 1, cardId: 1, evidence_proof: 'I mowed my leg', completed: false })
   await Activity.create({ studentId: 1, cardId: 1, evidence_proof: 'I mowed a lawn', completed: true })
-  // await Activity.create({ studentId: 1, cardId: 2, evidence_proof: 'I washed a lawn?', }) // DUPE!
+  await Activity.create({ studentId: 1, evidence_proof: 'I started a course' })
   done()
 }
 
